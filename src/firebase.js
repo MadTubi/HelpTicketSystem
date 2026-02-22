ec5e0f13480ef06fb0f27aad4faae589455525ab
Maddy Taylor <madtay324@gmail.com>
2026-02-22 14:42:56 -0600

    Add firebase emulator setup and env configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore,connectFirestoreEmulator } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIhAtGVbdQvmENXxT6AASOKohJZYliLLE",
-  authDomain: "mold-setup-dev.firebaseapp.com",
-  projectId: "mold-setup-dev",
-  storageBucket: "mold-setup-dev.firebasestorage.app",
-  messagingSenderId: "1094078495002",
-  appId: "1:1094078495002:web:12236b3841b7a214cd94a3"
+  apiKey: process.env.REACT_APPP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+if(process.env.REACT_APP_USE_EMULATOR === "true"){
+  connectFirestoreEmulator(db, "localhost", 8080)
+}
+
+export {db};
 
